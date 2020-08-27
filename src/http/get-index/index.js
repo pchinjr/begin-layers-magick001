4a7db14d76f9d551350a8a6cd1127ec2182370a3
Paul Chin Jr <pchinjr@gmail.com>
2020-08-26 20:30:49 -0400

    Update index.js

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -10,6 +10,7 @@ let body = `
 </body>
 `
   return {
+    statusCode: 200,
     headers: {
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
       'content-type': 'text/html; charset=utf8'
